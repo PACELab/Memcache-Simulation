bba8cd7f75b605fa6966d92bba45b27060c38273
Mansi <mansi.agarwal@stonybrook.edu>
2016-04-21 01:53:19 -0400

    add

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ all: ketama_sim.cpp k_time_sim.cpp sim.cpp
 	g++ -std=gnu++11 k_time_sim.cpp -o t_sim -lketama
 	g++ -std=gnu++11 sim.cpp -o sim
 
+ketama:
+	g++ -std=gnu++11 latency_ketama_sim.cpp -o kl_sim -lketama
+
 late: sim_latency.cpp
 	g++ -std=gnu++11 sim_latency.cpp -o l_sim
 
--- sim_latency.cpp
+++ sim_latency.cpp
@@ -1,17 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<climits>
-#include<fstream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <fstream>
+#include <algorithm>
+#include <unordered_map>
 
 #define SERVERS 10
 #define QUEUE 3000
 #define SIZE 1000000
+#define latency 1000
 using namespace std;
 
 main(){
 
+	//cache servers
 	vector<vector<long> > server (SERVERS);
+
+	//wait queue => latency time to fetch from DB
+	vector<long> wait_queue;
+
+	//hash map for the wait queue
+	std::unordered_map<long, long> wait_map;
 
 	ifstream trcfile ("zipf/B_1.txt");
 	long s;
@@ -20,9 +29,27 @@ main(){
 	long rem;
 
 	vector<long>::iterator it;
+	vector<long>::iterator wq;
 
 	if(trcfile.is_open()){
 		while(trcfile >> s){
+
+			//see if anything in wait queue is up for replacement 
+			if(wait_map.count(count) > 0){
+				//cout<<count<<"\t";
+				long f = wait_map[count];
+				long index = f % SERVERS;
+
+				//insert the item from wait queue
+				if(server[index].size() > QUEUE)
+					server[index].erase(server[index].begin());
+				server[index].push_back(f);
+
+				//delete the entry from hashmap
+				wait_map.erase(f);
+				wait_queue.erase(wait_queue.begin());
+
+			}
 
 			rem = s % SERVERS;
 			it = find(server[rem].begin(), server[rem].end(), s);
@@ -36,9 +63,19 @@ main(){
 				if(count >  SIZE/2)
 					miscount++;
 
-				if(server[rem].size() > QUEUE)
+				//find/insert the item in the 
+				wq = find(wait_queue.begin(), wait_queue.end(), s);
+				if(wq != wait_queue.end()){
+					;
+				} else{
+					wait_queue.push_back(s);
+					wait_map.emplace(count+latency, s);
+				}
+
+
+				/*if(server[rem].size() > QUEUE)
 					server[rem].erase(server[rem].begin());
-				server[rem].push_back(s);
+				server[rem].push_back(s);*/
 				
 			}
 
